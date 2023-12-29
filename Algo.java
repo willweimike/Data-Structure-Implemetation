2adec4316e8ba13c55e397eba5474214716b4956
Mike <mike20220101@gmail.com>
2023-12-29 13:33:41 +0800

    Update Algo.java

--- Algo.java
+++ Algo.java
@@ -4,8 +4,8 @@ public class Algo {
         int[] array = {3, 2, 10, 5, 1};
 
         quickSort(array, 0, array.length-1);
-        for (int i : array) {
-            System.out.println(i);
+        for (int element : array) {
+            System.out.println(element);
         }
     }
 
@@ -65,6 +65,51 @@ public class Algo {
             array[j+1] = temp;
         }
     }
+
+    public static void HeapSort(int[] arr) {
+        int n = arr.length;
+
+        // Build heap (rearrange array)
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+        // One by one extract an element from heap
+        for (int i = n - 1; i >= 0; i--) {
+            // Move current root to end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // call max heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+    // To heapify a subtree rooted with node i which is an index in arr
+    // n is size of heap
+    public static void heapify(int[] arr, int n, int i) {
+        int largest = i; // Initialize largest as root
+        int l = 2 * i; // left = 2*i
+        int r = 2 * i + 1; // right = 2*i + 1
+
+        // If left child is larger than root
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+
+        // If right child is larger than largest so far
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+
+        // If largest is not root
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            // Recursively heapify the affected subtree
+            heapify(arr, n, largest);
+        }
+    }
+    
 
     public static void mergeSort(int[] array) {
         int length = array.length;
